961de5ec50450d9f1c73a4e55c689b0a01e2c968
AD0791 <alexdisla1991@gmail.com>
2025-02-02 17:24:43 -0500

    moving forward with securing password

--- api/v1/http/handlers/sql/userSQLHandlers.go
+++ api/v1/http/handlers/sql/userSQLHandlers.go
@@ -1,18 +1,19 @@
 package sqlhandlers
 
 import (
+	sqlmodel "github.com/ad0791/todoServices/api/v1/models/sql_model"
+	"github.com/ad0791/todoServices/api/v1/repository/sqlrepository"
 	"github.com/ad0791/todoServices/api/v1/schema"
-	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/gofiber/fiber/v2/log"
 )
 
-var validate = validator.New()
+// validator define in the todo handler
 
-func hashPassword(password string) (string, error) {
-	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(b), err
-}
+//func hashPassword(password string) (string, error) {
+//	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+//	return string(b), err
+//}
 
 func RegisterUserRoutes(router fiber.Router) {
 	router.Post("/users", CreateUserHandler)
@@ -34,5 +35,46 @@ func RegisterUserRoutes(router fiber.Router) {
 // @Router      /users [post]
 func CreateUserHandler(c *fiber.Ctx) error {
 	var req schema.UserRequest
-	if err := c.BodyParser(&req)
+
+	// Parse incoming JSON
+	if err := c.BodyParser(&req); err != nil {
+		log.Errorf("Failed to parse request body (create user): %v", err)
+		return c.Status(fiber.StatusBadRequest).JSON(schema.ErrorMessage{Error: "Cannot parse JSON"})
+	}
+
+	// Validate the request
+	if err := validate.Struct(&req); err != nil {
+		log.Errorf("Validation failed (create user): %v", err)
+		return c.Status(fiber.StatusBadRequest).JSON(schema.ErrorMessage{Error: "Validation error"})
+	}
+
+	// If we were hashing:
+	// hashedPwd, err := hashPassword(req.Password)
+	// if err != nil {
+	// 	log.Errorf("Failed to hash password: %v", err)
+	// 	return c.Status(fiber.StatusInternalServerError).JSON(schema.ErrorMessage{Error: "Password hashing failed"})
+	// }
+
+	user := &sqlmodel.User{
+		FullName: req.FullName,
+		Email:    req.Email,
+		// Password: hashedPwd,
+		Password: req.Password, // Temporarily storing raw password (for now)
+	}
+
+	if err := sqlrepository.CreateUser(user); err != nil {
+		log.Errorf("Database error (create user): %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(schema.ErrorMessage{Error: "Database error"})
+	}
+
+	// Prepare response
+	resp := &schema.UserResponse{
+		ID:        user.ID,
+		FullName:  user.FullName,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt.Format("2006-01-02"),
+	}
+
+	log.Infof("User with ID %d created successfully", user.ID)
+	return c.Status(fiber.StatusCreated).JSON(resp)
 }
--- todo.md
+++ todo.md
@@ -1,16 +1,16 @@
-# V2 MODE
+# V1 security and role
 
 (A)  we create a user with fullname , email and password.
 (A) we need to hash&salt the password
 
-(A) User, role based access, we need to retrieve the create users and I the super admin should be able to give admin access to this USER. 
+(A) User, role based access, we need to retrieve the create users and I the super admin should be able to give admin access to this USER.
 (A) which means we need a way to create super admin
 (A) role( super admin, admin, user) // CRUD to give or remove this role to a user. 
 (A) a User should always have a  role and only one role.
 (A) user has many todos
 
 (A) a user can update is password (forgot password scenario)
-(A) we also need to introduce JWT, that token needs to expire in 1 week. and since we dont have a frontend, we need a token table. 
+(A) we also need to introduce JWT, that token needs to expire in 1 week. and since we dont have a frontend, we need a token table.
 (A) the super admin token expiration is 30 days.
 (A) admin and user token expiration is 7 days (1 week)
 
